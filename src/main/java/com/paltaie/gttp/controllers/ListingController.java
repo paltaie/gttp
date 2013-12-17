254fde76e043d9bf26ed7e74855fc5f205c3086d
Patrick Altaie <patrick.altaie@iconsolutions.com>
2013-12-17 00:00:55 +0000

    Using dem constants

--- src/main/java/com/paltaie/gttp/controllers/ListingController.java
+++ src/main/java/com/paltaie/gttp/controllers/ListingController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.cd.reddit.http.util.RedditApiResourceConstants;
 import com.cd.reddit.json.mapping.RedditLink;
 import com.google.gson.Gson;
 import com.paltaie.gttp.service.ThreadService;
@@ -21,7 +22,7 @@ public class ListingController {
 	@RequestMapping("/subreddit/{subreddit}.*")
 	public ModelAndView getSubreddit(@PathVariable("subreddit") String subreddit) {
 		ModelAndView mav = new ModelAndView("json/subreddit");
-		List<RedditLink> listing = threadService.getListing(subreddit, "hot");
+		List<RedditLink> listing = threadService.getListing(subreddit, RedditApiResourceConstants.HOT);
 		Gson gson = new Gson();
 		mav.addObject("string", gson.toJson(listing));
 		return mav;
