beb092170448adf864e33ebed8dabb9502e0529f
Patrick Altaie <patrick.altaie@iconsolutions.com>
2017-04-26 15:03:29 +0100

    @paltaie Remove unused files

--- src/main/java/com/paltaie/gttp/model/Data.java
+++
@@ -1,8 +0,0 @@
-package com.paltaie.gttp.model;
-
-import java.util.List;
-
-public class Data {
-	private String modhash;
-	List<Data> children;
-}
--- src/main/java/com/paltaie/gttp/service/ImageService.java
+++
@@ -1,5 +0,0 @@
-package com.paltaie.gttp.service;
-
-public interface ImageService {
-	byte[] generateScoreImage(String score, String threadUrl);
-}
