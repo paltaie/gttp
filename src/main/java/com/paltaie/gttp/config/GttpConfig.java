f2c37bc675d66a012b8fff7325e9308408125bda
Patrick Altaie <patrick.altaie@iconsolutions.com>
2017-05-09 17:12:32 +0100

    Remove weird way we're using to serialise JSON and use @ResponseBody instead

--- src/main/java/com/paltaie/gttp/config/GttpConfig.java
+++ src/main/java/com/paltaie/gttp/config/GttpConfig.java
@@ -5,11 +5,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.JstlView;
 import org.springframework.web.servlet.view.UrlBasedViewResolver;
 
 @Configuration
 @ComponentScan(basePackages = "com.paltaie.gttp")
+@EnableWebMvc
 public class GttpConfig {
 
     @Bean
--- src/test/java/com/paltaie/gttp/controllers/ListingControllerTest.java
+++ src/test/java/com/paltaie/gttp/controllers/ListingControllerTest.java
@@ -1,18 +1,17 @@
 package com.paltaie.gttp.controllers;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.paltaie.gttp.model.RedditLink;
 import com.paltaie.gttp.service.ThreadService;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
-import org.springframework.web.servlet.ModelAndView;
 
 import java.util.Arrays;
+import java.util.List;
 
+import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.ModelAndViewAssert.assertModelAttributeValue;
-import static org.springframework.test.web.ModelAndViewAssert.assertViewName;
 
 public class ListingControllerTest {
 
@@ -22,14 +21,13 @@ public class ListingControllerTest {
     @Before
     public void setUp() throws Exception {
         when(threadService.getListing("SUBREDDIT", "hot")).thenReturn(Arrays.asList(new RedditLink(), new RedditLink()));
-        listingController = new ListingController(threadService, new ObjectMapper());
+        listingController = new ListingController(threadService);
     }
 
     @Test
     public void getSubreddit() throws Exception {
-        ModelAndView mav = listingController.getSubreddit("SUBREDDIT");
-        assertViewName(mav, "json/subreddit");
-        assertModelAttributeValue(mav, "string", "[{\"id\":null,\"title\":null,\"ups\":0,\"downs\":0,\"url\":null,\"author\":null},{\"id\":null,\"title\":null,\"ups\":0,\"downs\":0,\"url\":null,\"author\":null}]");
+        List<RedditLink> redditLinks = listingController.getSubreddit("SUBREDDIT");
+        assertEquals(2, redditLinks.size());
     }
 
 }
